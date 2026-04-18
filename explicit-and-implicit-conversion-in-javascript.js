22cf56712f54ca18f7fa663d6f902bfb1760174a
Hero <sovereign.university20@gmail.com>
2026-04-18 12:52:00 -0500

    assignment completed

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -22,11 +22,23 @@ Use console.log() to clearly show the before-and-after type conversions.
 let result = "5" - 2;
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean("false" === "true"); /* Used Boolean() to convert the result of the comparison to a boolean value */
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5; /* Used Number() to convert the string from the age variable to a number */
 console.log("Total Age: " + totalAge);
+
+
+let totalYears = "50" - 12; /* The string "50" is converted to a number before performing the subtraction due to implicit type conversion */
+  console.log("Total Years: " + totalYears);
+
+  let monetaryValue = 500;
+  let stringValue = String(monetaryValue); /* Used String() to convert the number from the monetaryValue variable to a string */
+  console.log("Monetary Value: " + stringValue);
+  console.log("Type of Value: " + typeof stringValue); /* This will show that stringValue is now of type 'string' */
+
+let accumulatedValue = undefined;
+console.log("Accumulated Value: " + accumulatedValue); /* This will show NaN because undefined is not a number and cannot be used in arithmetic operations */
